2d214247222d1aad5e092f77c4084c591dcc9bc5
Talha Nuh Uymaz <talha_nuh@hotmail.com>
2025-07-09 11:19:35 +0300

    Navigation System

---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchScreen from '../screens/SearchScreen'; 
+import ProfileScreen from '../screens/ProfileScreen';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Search"
+                    component={SearchScreen}
+                    options={{
+                            tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="search" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                            tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="person" size={size} color={color} />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+};
+
+export default TabNavigator;
---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Search Screen</Text>
+        </View>
+
+    )
+};
+export default SearchScreen;
+
+const styles = StyleSheet.create({
+    container: { 
+        flex:1, 
+        justifyContent:'center', 
+        alignItems:'center'},
+})
