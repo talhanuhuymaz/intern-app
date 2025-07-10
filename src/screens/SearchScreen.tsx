1e8952c4e16dfcf35b193e67fa4eb2a55d70d0b4
Talha Nuh Uymaz <talha_nuh@hotmail.com>
2025-07-10 14:45:14 +0300

    Added Search Bar used State Method

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,19 +1,44 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 
 const SearchScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Search Screen</Text>
-        </View>
+  const [username, setUsername] = useState('');
 
-    )
+  const giveSearch = () => {
+    console.log("Searching for user:", username);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Search Screen</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Write your Github Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+
+      <Button title="Search" onPress={giveSearch} />
+    </View>
+  );
 };
+
 export default SearchScreen;
 
 const styles = StyleSheet.create({
-    container: { 
-        flex:1, 
-        justifyContent:'center', 
-        alignItems:'center'},
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    width: '50%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 20,
+    padding: 10,
+    margin:10
+  }
+});
+
