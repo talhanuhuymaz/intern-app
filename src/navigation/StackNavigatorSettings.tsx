b833c9cd26ac1546045f826f54c326b4f49bc27d
Talha Nuh Uymaz <talha_nuh@hotmail.com>
2025-07-11 16:41:25 +0300

    Used nested navigations added StackNavigator for Settings page

---
+++ src/navigation/StackNavigatorSettings.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfileScreen from '../screens/ProfileScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+export type ProfileStackParamList = {
+  ProfilePage: undefined;
+  Settings: undefined;
+};
+
+const Stack = createNativeStackNavigator<ProfileStackParamList>();
+
+const StackNavigatorSettings = () => {
+    return (
+            <Stack.Navigator >
+                <Stack.Screen
+                    name="ProfilePage"
+                    component={ProfileScreen}
+                    options={{ title: 'Profile' }}
+
+                />
+                <Stack.Screen
+                    name="Settings"
+                    component={SettingsScreen}
+                    options={{ title: 'Settings' }}
+                />
+        
+            </Stack.Navigator>
+    )
+};
+
+export default StackNavigatorSettings;
