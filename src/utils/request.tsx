70b708e5f459e76ea9c77f1850e6940cf96e121c
Talha Nuh Uymaz <talha_nuh@hotmail.com>
2025-07-17 16:54:13 +0300

    Added logging for API calls in request utility to see the difference using with debouncer

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,11 +1,13 @@
 const BASE_URL = "https://api.github.com";
 
 const request = async (url: string) => {
+  console.log("API call:", url);
   const response = await fetch(BASE_URL + url);
 
   if (!response.ok) throw new Error("API Error");
   
   return response.json();
+  
 };
 
 
