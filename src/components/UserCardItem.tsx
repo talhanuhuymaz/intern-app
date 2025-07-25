b40e05a7e0e33391c33219a25d01eb8c7f17f972
Talha Nuh Uymaz <talha_nuh@hotmail.com>
2025-07-25 16:45:25 +0300

    Added Touchable feature to the cards so when you click on user card it also send you to the repositories

--- src/components/UserCardItem.tsx
+++ src/components/UserCardItem.tsx
@@ -1,49 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { GitHubUser } from '../store/types/GitUserType'; 
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { SearchStackParamList } from '../navigation/StackNavigatorSearch';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { GitHubUser } from "../store/types/GitUserType";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { SearchStackParamList } from "../navigation/StackNavigatorSearch";
 
 type Props = {
-    user: GitHubUser;
-}
+  user: GitHubUser;
+};
 
 function UserCardItem({ user }: Props) {
-    const navigation = useNavigation<NativeStackNavigationProp<SearchStackParamList>>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<SearchStackParamList>>();
 
-    return (
-         <View style={styles.card}>
-        
-                  <View style={styles.userInfoRow}>
-                    <Image source={{ uri: user.avatar_url }} style={styles.avatar} />
-                    <Text style={styles.name}>{user.login}</Text>
-                  </View>
-        
-                  <TouchableOpacity
-                    style={styles.repoButton}
-                    onPress={() => navigation.navigate('Repositories', { username: user.login })}>
-                    <Text style={styles.repoButtonText}>Repositories</Text>
-                  </TouchableOpacity>
-                  
-                </View>
-    )
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate("Repositories", { username: user.login })}>
+      <View style={styles.card}>
+        <View style={styles.userInfoRow}>
+          <Image source={{ uri: user.avatar_url }} style={styles.avatar} />
+          <Text style={styles.name}>{user.login}</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.repoButton}
+          onPress={() =>
+            navigation.navigate("Repositories", { username: user.login })
+          }
+        >
+          <Text style={styles.repoButtonText}>Repositories</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 export default UserCardItem;
 
 const styles = StyleSheet.create({
   card: {
-    minWidth:400,
+    minWidth: 400,
     width: "100%",
     marginTop: 10,
     padding: 15,
     borderRadius: 10,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
   },
-   userInfoRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  userInfoRow: {
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   avatar: {
@@ -60,12 +64,12 @@ const styles = StyleSheet.create({
     marginTop: 5,
     paddingVertical: 6,
     paddingHorizontal: 12,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     borderRadius: 6,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
   },
   repoButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 14,
   },
 });
