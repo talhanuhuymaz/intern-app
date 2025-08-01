a3ce558ba34b817c3f4b1b7ea9da6b23a3bae973
Talha Nuh Uymaz <talha_nuh@hotmail.com>
2025-08-01 18:01:18 +0300

    Added a headerRight in SearchPage Stack, now you can go to the fav repos from search stack

--- src/navigation/StackNavigatorSearch.tsx
+++ src/navigation/StackNavigatorSearch.tsx
@@ -1,39 +1,63 @@
-import React from "react";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SearchScreen from "../screens/SearchScreen/SearchScreen";
-import RepoScreen from "../screens/RepoScreen";
+import { useNavigation } from "@react-navigation/native";
+import {createNativeStackNavigator,NativeStackNavigationProp} from "@react-navigation/native-stack";
+import React, { useCallback } from "react";
+import { TouchableOpacity } from "react-native";
+import { Text } from "react-native-gesture-handler";
+import FavoriteScreen from "../screens/FavoriteScreen";
 import RepositoryDetailsScreen from "../screens/RepoDetailScreen/RepoDetailsScreen";
+import RepoScreen from "../screens/RepoScreen";
+import SearchScreen from "../screens/SearchScreen/SearchScreen";
+import UserDetailsScreen from "../screens/UserDetailsScreen";
 
 export type SearchStackParamList = {
   SearchPage: undefined;
   Repositories: { username: string };
-  RepositoryDetails: { repo: any }; 
+  RepositoryDetails: { repo: any };
+  Favorites: undefined;
+  UserDetails: undefined
 };
 
 const Stack = createNativeStackNavigator<SearchStackParamList>();
 
 const StackNavigatorSearch = () => {
-    return (
-            <Stack.Navigator >
-                <Stack.Screen
-                    name="SearchPage"
-                    component={SearchScreen}
-                    options={{ title: 'Search' }}
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SearchPage"
+        component={SearchScreen}
+        options={{
+          title: "Search",
+          headerRight: () => (
+            <TouchableOpacity onPress={()=> navigation.navigate('Search',{screen:'Favorites'})}>
+              <Text>ᯓ★</Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Favorites"
+        component={FavoriteScreen}
+        options={{ title: "Favorites", headerTintColor: "black" }}
+      />
+      <Stack.Screen
+        name="Repositories"
+        component={RepoScreen}
+        options={{ title: "Repositories", headerTintColor: "black" }}
+      />
+      <Stack.Screen
+        name="RepositoryDetails"
+        component={RepositoryDetailsScreen}
+        options={{ title: "Repo Details", headerTintColor: "black" }}
+      />
+      <Stack.Screen
+        name="UserDetails"
+        component={UserDetailsScreen}
+        options={{ title: "User Details", headerTintColor: "black" }}
+      />
 
-                />
-                <Stack.Screen
-                    name="Repositories"
-                    component={RepoScreen}
-                    options={{ title: 'Repositories', headerTintColor: 'black' }}
-                />
-                  <Stack.Screen
-                    name="RepositoryDetails"
-                    component={RepositoryDetailsScreen}
-                    options={{ title: 'Repo Details',  headerTintColor: 'black' }}
-            />
-        
-            </Stack.Navigator>
-    )
+    </Stack.Navigator>
+  );
 };
 
-export default StackNavigatorSearch
+export default StackNavigatorSearch;
